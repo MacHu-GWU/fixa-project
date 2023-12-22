53c5e287c3e48015ae9a6b59680baf54b6d71e40
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-12-22 15:58:35 -0500

    aws_lambda_version_and_alias 0.2.1

--- fixa/aws/aws_lambda_version_and_alias.py
+++ fixa/aws/aws_lambda_version_and_alias.py
@@ -20,30 +20,46 @@ Usage example::
         get_last_published_version,
         publish_version,
         keep_n_most_recent_versions,
+        get_alias,
+        RoutingConfig,
+        DEFAULT_CANARY_INCREMENTS,
+        InvalidCanaryIncrementsError,
+        NextVersionMissingError,
+        get_alias_routing_config,
         deploy_alias,
         delete_alias,
+        new_routing_config_for_blue_green,
+        new_routing_config_for_canary,
     )
 
 This module is originally from https://github.com/MacHu-GWU/fixa-project/blob/main/fixa/aws/aws_lambda_version_and_alias.py
 """
 
+import dataclasses
 import typing as T
 
 import botocore.exceptions
 
 if T.TYPE_CHECKING:  # pragma: no cover
     from mypy_boto3_lambda import LambdaClient
+    from mypy_boto3_lambda.type_defs import (
+        FunctionConfigurationTypeDef,
+        AliasConfigurationResponseTypeDef,
+    )
 
-__version__ = "0.1.1"
+__version__ = "0.2.1"
 
 LATEST = "$LATEST"
 
 
+# ------------------------------------------------------------------------------
+# Version
+# ------------------------------------------------------------------------------
 def list_versions_by_function(
     lbd_client: "LambdaClient",
     func_name: str,
     max_items: int = 9999,
-) -> T.List[dict]:
+) -> T.List[T.Union[dict, "FunctionConfigurationTypeDef"]]:
     """
     List all lambda function versions. Return a list of detail dict.
     The order of the versions is not guaranteed.
@@ -163,6 +179,210 @@ def keep_n_most_recent_versions(
     return versions_to_delete
 
 
+# ------------------------------------------------------------------------------
+# Alias
+# ------------------------------------------------------------------------------
+def get_alias(
+    lbd_client: "LambdaClient",
+    func_name: str,
+    alias: str,
+) -> T.Optional[T.Union[dict, "AliasConfigurationResponseTypeDef"]]:
+    """
+    Get alias detail dict. If the alias doesn't exist, return None.
+    """
+    try:
+        return lbd_client.get_alias(FunctionName=func_name, Name=alias)
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "ResourceNotFoundException":
+            return None
+        else:  # pragma: no cover
+            raise e
+
+
+@dataclasses.dataclass
+class RoutingConfig:
+    """
+    Canary deployment routing config.
+
+    :param version1: version 1 identifier
+    :param version2: version 2 identifier, if None, then all traffic goes to version 1
+    :param version2_weight: version 2 weight, from 1 ~ 99.
+    """
+    version1: str = dataclasses.field()
+    version2: T.Optional[str] = dataclasses.field(default=None)
+    version2_weight: T.Optional[int] = dataclasses.field(default=None)
+
+    @property
+    def version1_weight(self) -> int:
+        """
+        :return: version 1 weight, from 1 ~ 100.
+        """
+        if self.version2_weight is None:
+            return 100
+        else:
+            return 100 - self.version2_weight
+
+    @classmethod
+    def from_alias_detail(cls, alias_detail: dict):
+        """
+        Extract routing config from ``lbd_client.get_alias()`` API response.
+        """
+        version1 = alias_detail["FunctionVersion"]
+        additional_version_weights = alias_detail.get("RoutingConfig", {}).get(
+            "AdditionalVersionWeights", {}
+        )
+        if additional_version_weights:
+            version2, version2_weight = list(additional_version_weights.items())[0]
+            return cls(
+                version1=version1,
+                version2=version2,
+                version2_weight=round(version2_weight * 100),
+            )
+        else:
+            return cls(version1=version1)
+
+    def to_update_alias_kwargs(self) -> dict:
+        """
+        Generate corresponding keyword arguments for ``lbd_client.update_alias()``
+        or ``lbd_client.create_alias()``.
+        """
+        kwargs = dict(FunctionVersion=self.version1)
+        if self.version2 is not None:
+            kwargs["RoutingConfig"] = dict(
+                AdditionalVersionWeights={
+                    self.version2: round(self.version2_weight / 100, 2)
+                }
+            )
+        return kwargs
+
+    def to_deploy_alias_kwargs(self) -> dict:
+        """
+        Generate corresponding keyword arguments for :func:`deploy_alias`
+        """
+        kwargs = dict(version1=self.version1)
+        if self.version2 is not None:
+            kwargs["version2"] = self.version2
+            kwargs["version2_percentage"] = round(self.version2_weight / 100, 2)
+        return kwargs
+
+
+def _blue_green(next_version: str) -> RoutingConfig:  # pragma: no cover
+    """
+    Find the routing config for next blue-green deployment.
+
+    :param next_version: if publish a new version, what is the version number should be?
+
+    :return: :class:RoutingConfig
+    """
+    return RoutingConfig(version1=str(next_version))
+
+
+DEFAULT_CANARY_INCREMENTS = [25, 50, 75]
+
+
+class InvalidCanaryIncrementsError(ValueError):  # pragma: no cover
+    pass
+
+
+class NextVersionMissingError(ValueError):  # pragma: no cover
+    pass
+
+
+def _canary(
+    canary_increments: T.Optional[T.List[int]] = None,
+    next_version: T.Optional[str] = None,
+    routing_config: T.Optional[RoutingConfig] = None,
+) -> RoutingConfig:  # pragma: no cover
+    """
+    Find the routing config for next canary deployment.
+
+    :param canary_increments: a list of 1 ~ 100 integers, the weight of
+        the major version will be gradually increased by these numbers.
+    :param next_version: if publish a new version, what is the version number should be?
+    :param routing_config: existing routing config, use None to indicate
+        no existing routing config.
+
+    :return: :class:RoutingConfig
+    """
+    if canary_increments is None:
+        canary_increments = DEFAULT_CANARY_INCREMENTS
+
+    if len(canary_increments) == 0:
+        raise InvalidCanaryIncrementsError
+    elif any([(i <= 1 or i >= 99) for i in canary_increments]):
+        raise InvalidCanaryIncrementsError
+    else:
+        canary_increments.sort()
+
+    # it's the first deployment, route all traffic to the next version
+    if routing_config is None:
+        if next_version is None:
+            raise NextVersionMissingError(
+                "next_version must be provided for the first deployment"
+            )
+        return RoutingConfig(version1=str(next_version))
+    # it is in STABLE state (only one version), create a new canary deployment
+    elif routing_config.version2 is None:
+        if next_version is None:
+            raise NextVersionMissingError(
+                "next_version must be provided for a new canary deployment"
+            )
+        return RoutingConfig(
+            version1=next_version,
+            version2=routing_config.version1,
+            version2_weight=100 - canary_increments[0],
+        )
+    # it is not in STABLE state (only one version), gradually increase major version to 100%
+    else:
+        for threshold in canary_increments:
+            if routing_config.version1_weight < threshold:
+                return RoutingConfig(
+                    version1=routing_config.version1,
+                    version2=routing_config.version2,
+                    version2_weight=100 - threshold,
+                )
+        return RoutingConfig(
+            version1=routing_config.version1,
+        )
+
+
+def get_alias_routing_config(
+    lbd_client: "LambdaClient",
+    func_name: str,
+    alias: str,
+) -> T.Optional[RoutingConfig]:
+    """
+    The original get_alias API returns in this format::
+
+        {
+            'AliasArn': 'string',
+            'Name': 'string',
+            'FunctionVersion': 'string',
+            'Description': 'string',
+            'RoutingConfig': {
+                'AdditionalVersionWeights': {
+                    'string': 0.9
+                }
+            },
+            'RevisionId': 'string'
+        }
+
+    We would like to convert into a object to show the version and weight::
+
+        RoutingConfig(version1=..., version2=..., version2_weight=...)
+
+    :param lbd_client: ``boto3.client("lambda")`` object.
+    :param func_name: lambda function name
+    :param alias: alias name
+
+    :return: :class:`RoutingConfig`, if the alias doesn't exist, return None
+    """
+    res = get_alias(lbd_client, func_name, alias)
+    if res is None: # pragma: no cover
+        return res
+    return RoutingConfig.from_alias_detail(res)
+
+
 def deploy_alias(
     lbd_client: "LambdaClient",
     func_name: str,
@@ -200,6 +420,8 @@ def deploy_alias(
     if version1 is not None:
         version1 = str(version1)
     target_version = LATEST if version1 is None else version1
+
+    # figure out the secondary version and routing configuration
     if version2 is not None:
         version2 = str(version2)
         if not (0.01 <= version2_percentage <= 0.99):  # pragma: no cover
@@ -255,8 +477,60 @@ def delete_alias(
 ) -> dict:
     """
     The original API is already idempotent, so no need to check if the alias exists.
+
+    :param lbd_client: ``boto3.client("lambda")`` object.
+    :param func_name: lambda function name
+    :param alias: alias name
     """
     return lbd_client.delete_alias(
         FunctionName=func_name,
         Name=alias,
+    )
+
+
+def new_routing_config_for_blue_green(
+    lbd_client: "LambdaClient",
+    func_name: str,
+) -> RoutingConfig:
+    """
+    Find the routing config for next blue-green deployment.
+    """
+    last_published_version = get_last_published_version(lbd_client, func_name)
+    if last_published_version is None:
+        next_version = str(1)
+    else:
+        next_version = str(last_published_version + 1)
+    return _blue_green(next_version)
+
+
+def new_routing_config_for_canary(
+    lbd_client: "LambdaClient",
+    func_name: str,
+    alias: str,
+    canary_increments: T.Optional[T.List[float]] = None,
+) -> RoutingConfig:
+    """
+    Find the routing config for next canary deployment.
+
+    It assumes that your app version is from 1, 2, 3, ... and so on.
+    For canary deployment, either you route 100% traffic to a version, either
+    you route x%, y% to a major version and a minor version. The concept of
+    major version is the target version you, it starts from very low percentage
+    of traffic, and gradually increase to 100%.
+
+    The concept of ``next_version`` is that
+    "if publish a new version, what is the version number should be?".
+    For example, if you haven't made any deployment yet, then the next version is "1".
+    If you have deployed version "1", then the next version is "2" (which does not exists yet).
+    """
+    last_published_version = get_last_published_version(lbd_client, func_name)
+    if last_published_version is None:
+        next_version = str(1)
+    else:
+        next_version = str(last_published_version + 1)
+    routing_config = get_alias_routing_config(lbd_client, func_name, alias)
+    return _canary(
+        canary_increments,
+        next_version,
+        routing_config,
     )
--- tests_aws/test_aws_lambda_version_and_alias.py
+++ tests_aws/test_aws_lambda_version_and_alias.py
@@ -10,8 +10,13 @@ from fixa.aws.aws_lambda_version_and_alias import (
     version_dct_to_version_int,
     publish_version,
     keep_n_most_recent_versions,
+    get_alias,
+    get_alias_routing_config,
     deploy_alias,
     delete_alias,
+    RoutingConfig,
+    new_routing_config_for_blue_green,
+    new_routing_config_for_canary,
 )
 
 aws_profile = "awshsh_app_dev_us_east_1"
@@ -20,6 +25,10 @@ alias = "LIVE"
 
 boto_ses = boto3.session.Session(profile_name=aws_profile)
 lbd_client = boto_ses.client("lambda")
+
+
+def _test_setup():
+    delete_alias(lbd_client, func_name, alias)
 
 
 def _test_publish_version():
@@ -32,11 +41,11 @@ def _test_publish_version():
         FunctionName=func_name,
         Environment={
             "Variables": {
-                "CHORE": uuid.uuid4().hex,
+                "md5": uuid.uuid4().hex,
             }
         },
     )
-    time.sleep(10)
+    time.sleep(5)
     is_new_version_created, version2 = publish_version(lbd_client, func_name)
     assert is_new_version_created is True
     assert version2 == version1 + 1
@@ -84,13 +93,77 @@ def _test_deploy_alias():
 def _test_delete_alias():
     delete_alias(lbd_client, func_name, alias)
     delete_alias(lbd_client, func_name, alias)
+    assert get_alias(lbd_client, func_name, alias) is None
+
+
+def _test_deployment_pattern():
+    def _publish_version():
+        lbd_client.update_function_configuration(
+            FunctionName=func_name,
+            Environment={
+                "Variables": {
+                    "md5": uuid.uuid4().hex,
+                }
+            },
+        )
+        time.sleep(5)
+        publish_version(lbd_client, func_name)
+
+    def _deploy_alias(rc: RoutingConfig):
+        deploy_alias(
+            lbd_client,
+            func_name,
+            alias,
+            **rc.to_deploy_alias_kwargs(),
+        )
+
+    # blue/green, create new version
+    rc = new_routing_config_for_blue_green(lbd_client, func_name)
+    _publish_version()
+    _deploy_alias(rc)
+    rc1 = get_alias_routing_config(lbd_client, func_name, alias)
+
+    # blue/green, create new version, version + 1
+    rc = new_routing_config_for_blue_green(lbd_client, func_name)
+    _publish_version()
+    _deploy_alias(rc)
+    rc2 = get_alias_routing_config(lbd_client, func_name, alias)
+    assert int(rc2.version1) == int(rc1.version1) + 1
+
+    # canary,
+    rc = new_routing_config_for_canary(lbd_client, func_name, alias)
+    _publish_version()
+    _deploy_alias(rc)
+    rc3 = get_alias_routing_config(lbd_client, func_name, alias)
+    assert rc3 == rc
+    assert rc3.version1_weight == 25
+
+    rc = new_routing_config_for_canary(lbd_client, func_name, alias)
+    _deploy_alias(rc)
+    rc3 = get_alias_routing_config(lbd_client, func_name, alias)
+    assert rc3 == rc
+    assert rc3.version1_weight == 50
+
+    rc = new_routing_config_for_canary(lbd_client, func_name, alias)
+    _deploy_alias(rc)
+    rc3 = get_alias_routing_config(lbd_client, func_name, alias)
+    assert rc3 == rc
+    assert rc3.version1_weight == 75
+
+    rc = new_routing_config_for_canary(lbd_client, func_name, alias)
+    _deploy_alias(rc)
+    rc3 = get_alias_routing_config(lbd_client, func_name, alias)
+    assert rc3 == rc
+    assert rc3.version1_weight == 100
 
 
 def test():
+    _test_setup()
     _test_publish_version()
     _test_keep_n_most_recent_versions()
     _test_deploy_alias()
     _test_delete_alias()
+    _test_deployment_pattern()
 
 
 if __name__ == "__main__":
